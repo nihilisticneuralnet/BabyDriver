cb05d7a85e6689cb180025972ef283f8f93a91ce
𝘱𝘢𝘳𝘵𝘩𝘦𝘯𝘰𝘴 <138315505+nihilisticneuralnet@users.noreply.github.com>
2026-01-25 00:42:09 +0530

    Update track.py

--- track.py
+++ track.py
@@ -83,3 +83,52 @@ class tracker:
         del self.center_points[object_id]
         if object_id in self.disappeared:
             del self.disappeared[object_id]
+            
+            D = np.linalg.norm(np.array(object_centroids)[:, np.newaxis] - np.array([(c[0], c[1]) for c in input_centroids]), axis=2)
+            
+            rows = D.min(axis=1).argsort()
+            cols = D.argmin(axis=1)[rows]
+            
+            used_row_indices = set()
+            used_col_indices = set()
+            
+            for (row, col) in zip(rows, cols):
+                if row in used_row_indices or col in used_col_indices:
+                    continue
+                
+                if D[row, col] <= 50:
+                    object_id = object_ids[row]
+                    self.center_points[object_id] = (input_centroids[col][0], input_centroids[col][1])
+                    objects_bbs_ids.append([input_centroids[col][2], input_centroids[col][3], 
+                                          input_centroids[col][4], input_centroids[col][5], 
+                                          object_id, input_centroids[col][6]])
+                    
+                    used_row_indices.add(row)
+                    used_col_indices.add(col)
+                    
+                    if object_id in self.disappeared:
+                        del self.disappeared[object_id]
+            
+            unused_row_indices = set(range(0, D.shape[0])) - used_row_indices
+            unused_col_indices = set(range(0, D.shape[1])) - used_col_indices
+            
+            if D.shape[0] >= D.shape[1]:
+                for row in unused_row_indices:
+                    object_id = object_ids[row]
+                    self.disappeared[object_id] = self.disappeared.get(object_id, 0) + 1
+                    if self.disappeared[object_id] > self.max_disappeared:
+                        self.deregister(object_id)
+            else:
+                for col in unused_col_indices:
+                    cx, cy, x, y, w, h, index = input_centroids[col]
+                    self.center_points[self.id_count] = (cx, cy)
+                    objects_bbs_ids.append([x, y, w, h, self.id_count, index])
+                    self.id_count += 1
+        
+        return objects_bbs_ids
+    
+    def deregister(self, object_id):
+        del self.center_points[object_id]
+        if object_id in self.disappeared:
+            del self.disappeared[object_id]
+
